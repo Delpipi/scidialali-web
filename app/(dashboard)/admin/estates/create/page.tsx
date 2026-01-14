e08379ff55325ef2d57568cd8f3000bc81094361
Delpipi <alexandrepaulkouame@gmail.com>
2026-01-14 22:51:30 +0000

    fix run code error by updating CreateEstateForm

--- app/(dashboard)/admin/estates/create/page.tsx
+++ app/(dashboard)/admin/estates/create/page.tsx
@@ -20,7 +20,7 @@ export default async function Page() {
         ]}
       />
       <div>
-        <CreateEstateForm gestionnaires={[]} />
+        <CreateEstateForm />
       </div>
     </div>
   );
